45be41d00d0a516066ac5794534d768ee54193a1
Carlos Brito <cbrito1994@gmail.com>
2021-08-11 12:58:05 -0500

    created the store file from redux and added it to the app

--- client/src/App.js
+++ client/src/App.js
@@ -9,6 +9,7 @@ import {
 import { setContext } from '@apollo/client/link/context';
 
 import { Provider } from 'react-redux';
+import store from './utils/store';
 
 import Home from './pages/Home';
 import Detail from './pages/Detail';
@@ -16,7 +17,6 @@ import NoMatch from './pages/NoMatch';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
 import Nav from './components/Nav';
-import { StoreProvider } from './utils/GlobalState';
 import Success from './pages/Success';
 import OrderHistory from './pages/OrderHistory';
 
