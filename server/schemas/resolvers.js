e4bd577ad1bc335d458cfb655b204cc653f1a303
Carlos Brito <cbrito1994@gmail.com>
2021-08-21 21:41:00 -0500

    Final commit

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -90,7 +90,7 @@ const resolvers = {
     }
   },
   Mutation: {
-    addUser: async (parent, args) => {
+    addProfile: async (parent, args) => {
       const user = await User.create(args);
       const token = signToken(user);
 
